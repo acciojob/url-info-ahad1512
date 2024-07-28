60184874e388d37db1f2cb63ad7b401f9e710157
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-07-29 02:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+function calcSize(str) {
+	let size = str.length;
+	return "The length of the URL is: "+size;
+}
+let str =window.location.href;
+alert(calcSize(str));
